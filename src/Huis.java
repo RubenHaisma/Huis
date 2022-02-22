8e0a7c640769d48ada827943e871d5083b689188
Ruben Haisma <rubenhaisma@icloud.com>
2022-02-22 12:34:42 +0100

    Initial commit

---
+++ src/Huis.java
@@ -0,0 +1,23 @@
+public class Huis {
+    private String adres;
+    private int bouwjaar;
+    private Persoon huisbaas;
+
+    public Huis(String adr, int bwjr) {
+        this.adres = adr;
+        this.bouwjaar = bwjr;
+    }
+
+    public void setHuisbaas(Persoon hb) {
+        this.huisbaas = hb;
+    }
+
+    public Persoon getHuisbaas() {
+        return huisbaas;
+    }
+
+    public String toString() {
+        return "Huis " + adres + " is gebouwd in " + bouwjaar + "\nen heeft huisbaas " + getHuisbaas();
+    }
+
+}
